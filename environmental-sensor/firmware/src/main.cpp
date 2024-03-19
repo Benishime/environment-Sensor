374f1312ce2bbdf68d7f779e672103d0d5efe172
Ahmet Taha Uğur <104079765+Benishime@users.noreply.github.com>
2024-03-19 16:08:15 +0300

    Add files via upload

---
+++ environmental-sensor/firmware/src/main.cpp
@@ -0,0 +1,103 @@
+#include <Arduino.h>
+#include <HardwareSerial.h>
+const int sensorPinRX = 16; //RXD2
+const int sensorPinTX = 17; //TXD2
+
+char array[18]; 
+char Sendcalled(char sendByte[9]);
+char SendByte[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79};
+char ReceiveByte[25];
+int ReceivebytePlace = 0;
+int place = 0;
+bool StartCalled(String readData);
+bool StopCalled(String readData);
+String ReadMassage();
+String ReceivedByte();
+
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial2.begin(9600, SERIAL_8N1, 16, 17);
+}
+
+void loop() {
+  String receivedData = ReadMassage();
+  bool startCalled = StartCalled(receivedData);
+  bool stopCalled = StopCalled(receivedData);
+  if (startCalled && !stopCalled) {
+    Sendcalled(SendByte);
+    ReceivedByte();
+  } else if (startCalled && stopCalled) {
+    Serial.println("stop");
+  }
+
+  delay(1000);
+}
+
+String ReadMassage() {
+  char inbyte;
+  bool read_start = false;
+
+  while (Serial.available() > 0) {
+    inbyte = Serial.read();
+
+    if (inbyte == '#') {
+      read_start = true;
+      place = 0;
+    } else if ((read_start==true) && (inbyte != '!')) {
+      array[place] = inbyte;  
+      place++;
+    } else if ((read_start==true) && (inbyte == '!')) {
+      array[place] = '\0';
+      if (String(array) == "start" || String(array) == "stop") {
+        return String(array);
+      }
+    }
+  } 
+  return "";   
+}
+
+bool StartCalled(String readData) {
+  if (readData == "start") {
+    return true;
+  }
+  return false;
+}
+
+bool StopCalled(String readData) {
+  if (readData == "stop") {
+    return true;
+  }
+  return false;
+}
+
+char Sendcalled(char sendByte[9]) {
+  delay(1000);
+  char readByte;
+   bool read_start = false;
+  char asd[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79};
+  for(int i=0;i<9;i++){
+    Serial2.write(asd[i]);
+    Serial.println("Byte yollandı");
+  }
+
+  delay(1000);
+
+  while(Serial2.available()>0){
+    readByte =Serial2.read();
+    if(readByte == 0xFF){
+      read_start = true;
+      ReceivebytePlace = 0;
+    }else if(read_start == true){
+      ReceiveByte[ReceivebytePlace] = readByte;
+      ReceivebytePlace++;
+    }
+    Serial.println("byte alındı");
+    Serial.println(readByte);
+  }
+  delay(1000);
+}
+
+String ReceivedByte(){
+}
